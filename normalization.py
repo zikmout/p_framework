6818c5187c03657c067b3483eaf78ab594439d8b
X <xxx@MacBook-Pro-de-X.local>
2017-11-25 17:17:19 +0100

    sending autoencoder class

--- normalization.py
+++ normalization.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
 import sklearn
+import category_encoders as ce
 
 from sklearn import preprocessing as skp
 
-Class AutoEncoder:
+Class Normalization:
     
     def __init__(self, input_df):
         self.input_df = input_df
@@ -13,15 +14,18 @@ Class AutoEncoder:
         self.item = pd.Series(list())
 
     def get_imitem(self):
+        print(self.item.info())
+        return self.item
         
     def purge_imitems(self):
         self.output_df = np.concat([self.output_df, self.imitems])
         del.self.imitems()
     
-    def encode(self, label, method, sparse_output=False):
+    def auto_encoder(self, label, method, sparse_output=False):
         # self.item = self.input_df[label]
         main_method = method.split('_')[0]
         sub_method = method.split('_')[1]
+        
         if main_method == 'binary':
             self.item = self.input_df[label]
             if sub_method == 'dependant': lb = skp.LabelBinaryizer(neg_label=-1, \
@@ -31,13 +35,11 @@ Class AutoEncoder:
             
             lb.fit(self.item)
             if len(lb.classes_) != 2:
-                u.echo_debug('Label \'{}\' cannot be binary_dependant \
-                encoded:\nClasses are :\n{}'.format(label, lb.classes_))
+                u.echo_debug('Label ... cannot be binary_dependant encoded. Classes are :') # .format(label, lb.classes_))
             if sparse_output:
                 lb.transform()
                 return self.item
-            else:
-                return self.item
+            return self.item
         
         else if main_method == 'numerical':
             self.item = self.input_df[label]
@@ -47,5 +49,31 @@ Class AutoEncoder:
                 self.item = scaler.scale_
             else if sub_method =='independant':
                 self.item = pd.get_dummies(data=self.input_df, columns=label)
-                self.item = scaler.transform(self)    
+                self.item = scaler.transform(self.item)    
             return self.item
+
+        else if main_method == 'categorical'
+            # self.item = self.input_df[label]
+
+            if sub_method == 'dependant_le':
+                print('Debug -> {}'.format(self.item.values_counts()))
+                self.item = pd.get_dummies(data=self.input_df, columns=label)
+                le = skp.LabelEncoder()
+                self.item = le.fit_transform(self.item)
+                # or df.apply(LabelEncoder().fit_transform)
+                return self.item
+            else if sub_method == 'dependant_bd':
+                self.item = self.input_df.select_dtypes(include=[label]).copy()
+                bd = ce.backward_difference.BackwardDifferenceEncoder(cols=[.join(label,'_type')])
+                bd.fit(self.item, verbose=1)
+                return self.item
+                # show ? bd.transform(self.item).iloc[]
+            else if sub_method == 'dependant_pe':
+                pe = ce.polynomialEncoder(cols=.join(label,'_type'))
+                pe.fit(self.item, verbose=1)
+                return self.item
+
+            echo_debug('Vous n etes pas cense etre la.')
+    
+    
+                # show ? pee.transform(self.item).iloc[]
